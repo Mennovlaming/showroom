526deaf3f0299968fdf93739315196d3f47c8cb4
Menno Vlaming <mennovlaming@macbook-pro-van-menno.home>
2024-03-24 18:37:34 +0100

    redirectfix5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 //pages & components
 import Home from './pages/Home/Home.jsx'
